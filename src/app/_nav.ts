0c8b598bfef55e066f55debe1899cc2f9e151b6f
danielkong <daniel_kong@datahouse.com>
2018-04-02 14:58:37 -1000

    first commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,15 @@
+export const navigation = [
+  {
+    title: true,
+    name: 'Welcome'
+  },
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  }
+];
